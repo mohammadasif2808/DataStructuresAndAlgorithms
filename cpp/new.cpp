11c4d4d4ec769063a5e6204750362b151948765c
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-08-15 22:25:40 +0530

    initial commit

---
+++ cpp/new.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("../files/input.txt", "r", stdin);
+    freopen("../files/output.txt", "w", stdout);
+#endif
+   int x;
+    cin >> x;
+    cout << "hello world from c+++ ! " << x;
+}
---
+++ java/PairSumProblem.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class PairSumProblem {
+    public static void main(String[] args) throws Exception {
+//        if (System.getProperty("ONLINE_JUDGE") == null) {
+//            System.setIn(new FileInputStream("../files/input.txt"));
+//            System.setOut(new PrintStream(new FileOutputStream("../files/output.txt")));
+//        }
+
+        // Array
+        int arr[] = { 2, 1, 8, 6, -2, 5, 3, 0 };
+
+        int k = 8;
+        int size = arr.length;
+
+        // Solution 1 Using nested for loop - O(N^2)
+
+        // for (int i = 0; i < size; i++) {
+        //     for (int j = 0; j < size; j++) {
+        //         if (arr[i] + arr[j] == k) {
+        //             System.out.println(arr[i] + " " + arr[j]);
+        //         }
+        //     }
+        // }
+
+        System.out.println();
+
+        // Solution 2 Using HashSet - O(N)
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < size; i++) {
+            set.add(arr[i]);
+        }
+
+        // System.out.println("Set: " + set);
+
+        for (int i = 0; i < size; i++) {
+            int find = k - arr[i];
+            if (set.contains(find)) {
+                System.out.println(arr[i] + " " + find);
+            }
+        }
+
+        // Print set elements
+        // Iterator<Integer> iterator = set.iterator();
+        // while (iterator.hasNext()) {
+        // Integer type = iterator.next();
+        // System.out.println(type);
+        // }
+
+        // Print arrary using for each loop
+        // System.out.println();
+        // print array
+        // for(int x : arr) {
+        // System.out.print(x+" ");
+        // }
+
+        // Simple input and output
+        // Scanner sc = new Scanner(System.in);
+        // int x;
+        // x=sc.nextInt();
+        // System.out.print("Hello world from Java !!! "+x);
+    }
+}
